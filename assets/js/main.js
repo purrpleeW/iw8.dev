d83cbc74ebf8082b3b706f8008a66469722450a9
purrplee <purrplee28@gmail.com>
2026-03-30 12:37:55 +1300

    added some files for github pages

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function enterSite() {
+    alert("site is still being worked on :3");
+}
+
+document.querySelector(".cta").addEventListener("click", enterSite);
+
+document.addEventListener("keydown", enterSite);
